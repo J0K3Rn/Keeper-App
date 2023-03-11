c1ab6dace48fd5f65d5cda19e829111fc57a7863
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-03-11 00:00:07 -0600

    Dynamically generating notes

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "./notes.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />)};
       <Footer />
     </div>
   );
